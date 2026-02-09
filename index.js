4f42597d22e1667d1d2b34bdee1bf6da883b15e5
tsega b <tsegabogale92@gmail.com>
2026-02-09 23:51:55 +0300

    add file metadata microservice

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse' , multer().single('upfile') , (req , res) => {
-  res.json({
-    name : req.file.originalname,
-    type : req.file.mimetype,
-    size : req.file.size
-  })
-}
-)
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
